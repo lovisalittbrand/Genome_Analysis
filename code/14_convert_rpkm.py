825a9ba68c1abe524f0aa29eeaac08694a8182c8
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-05-21 15:27:24 +0200

    scripts rpkm v2

--- code/14_convert_rpkm.py
+++ code/14_convert_rpkm.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 import sys
-import numpy as np
-import re
 
 #This script calculates the gene lengths and thereafter computes the RPKM for each feature
 
 #Creating result file
-with open(result_file, "a") as file:
-    file.write('Feature_ID' + ' ' + 'RPKM' + '\n')
+result_file = 'result_count.txt'
 
 # Importing files:
 #  1. File with annotation to access gene length
@@ -18,27 +15,33 @@ file_count = open(sys.argv[2], "r")
 # Calculating the gene lengths for mapped features
 file_gff_lines = file_gff.readlines()
 file_count_lines = file_count.readlines()
+
 for line in file_count_lines:
-    # Split at spacing
-    split_line_count = line.split()
-    feature_name = split_line_count[0]
-    feature_count = int(split_line_count[1])
+ if not line.startswith('_'):
+  # Split at spacing
+  split_line_count = line.split()
+  feature_name = split_line_count[0]
+  feature_count = int(split_line_count[1])
 
-    for line2 in file_gff_lines:
-        split_line_gff = line.split()
-        if not line2.startswith('##'):
-            feature_info = split_line_gff[8]
-            feature_info_split = feature_info.split(";")
-            feature_id = feature_info.split("=")
-            feature_id = feature_id[1]
+  for line2 in file_gff_lines:
+   if line2.startswith('k'):
+    split_line_gff = line2.split('\t')
+    feature_info = split_line_gff[8]
+    feature_id = feature_info.split("ID=")
+    
+    with open(result_file, "a") as file:
+      file.write(feature_id[1])
+    
 
-            # Matching ID in both files
-            if feature_id == feature_name:
-                gene_length = int(split_line_gff[4]) - int(split_line_gff[3])
+    # Matching ID in both files
+    if feature_id[1].startswith(feature_name):
+     gene_length = int(split_line_gff[4]) - int(split_line_gff[3])
+     with open(result_file, "a") as file:
+      file.write('aaa')
 
-                # Running for all data from site D1
-                ks = 0.873799
-                rpkm = (feature_count/ks)/gene_length
-                with open(result_file, "a") as file:
-                    file.write(feature_name + ' ' + rpkm)
+     # Running for all data from site D1
+     ks = 0.873799
+     rpkm = (feature_count/ks)/gene_length
+     with open(result_file, "a") as file:
+      file.write(feature_name + ' ' + rpkm)
 
