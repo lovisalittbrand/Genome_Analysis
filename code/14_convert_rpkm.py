e47e1cb9d1274f8190a2f8b343a30661b0f2a36e
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-05-21 23:57:57 +0200

    added updated rpkm scripts

--- code/14_convert_rpkm.py
+++ code/14_convert_rpkm.py
@@ -4,7 +4,7 @@ import sys
 #This script calculates the gene lengths and thereafter computes the RPKM for each feature
 
 #Creating result file
-result_file = 'result_count.txt'
+result_file = sys.argv[3]
 
 # Importing files:
 #  1. File with annotation to access gene length
@@ -23,7 +23,7 @@ for line in file_count_lines:
   # Split at spacing
   split_line_count = line.split()
   feature_name = split_line_count[0]
-  feature_count = float(split_line_count[1])
+  feature_count = split_line_count[1]
 
   for line2 in file_gff_lines:
    if line2.startswith('k'):
@@ -31,10 +31,9 @@ for line in file_count_lines:
     feature_info = split_line_gff[8]
     feature_id = feature_info.split("ID=")
     if (len(feature_id) > 1):
-     with open(result_file, "a") as file:
-       file.write(feature_id[1])
+     #with open(result_file, "a") as file:
+      #file.write(feature_id[1])
     
-
      # Matching ID in both files
      if feature_id[1].startswith(feature_name):
       gene_length = float(split_line_gff[4]) - float(split_line_gff[3])
@@ -42,7 +41,8 @@ for line in file_count_lines:
 
       # Running for all data from site D1
       ks = 0.873799
-      rpkm = (feature_count/ks)/gene_length
+      rpkm = (float(feature_count)/ks)
+      rpkm2 = rpkm/gene_length
       with open(result_file, "a") as file:
-       file.write(str(rpkm))
+       file.write(feature_name + ' ' + str(rpkm2) + '\n')
 
