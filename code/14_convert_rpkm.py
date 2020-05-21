c230dd2ca57dedb1f84b5f3b0fe71266387ddbd4
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-05-21 22:18:04 +0200

    modified script rpkm

--- code/14_convert_rpkm.py
+++ code/14_convert_rpkm.py
@@ -17,31 +17,32 @@ file_gff_lines = file_gff.readlines()
 file_count_lines = file_count.readlines()
 
 for line in file_count_lines:
- if not line.startswith('_'):
+ if line.startswith('_'):
+  continue
+ else:
   # Split at spacing
   split_line_count = line.split()
   feature_name = split_line_count[0]
-  feature_count = int(split_line_count[1])
+  feature_count = float(split_line_count[1])
 
   for line2 in file_gff_lines:
    if line2.startswith('k'):
     split_line_gff = line2.split('\t')
     feature_info = split_line_gff[8]
     feature_id = feature_info.split("ID=")
-    
-    with open(result_file, "a") as file:
-      file.write(feature_id[1])
+    if (len(feature_id) > 1):
+     with open(result_file, "a") as file:
+       file.write(feature_id[1])
     
 
-    # Matching ID in both files
-    if feature_id[1].startswith(feature_name):
-     gene_length = int(split_line_gff[4]) - int(split_line_gff[3])
-     with open(result_file, "a") as file:
-      file.write('aaa')
+     # Matching ID in both files
+     if feature_id[1].startswith(feature_name):
+      gene_length = float(split_line_gff[4]) - float(split_line_gff[3])
+     
 
-     # Running for all data from site D1
-     ks = 0.873799
-     rpkm = (feature_count/ks)/gene_length
-     with open(result_file, "a") as file:
-      file.write(feature_name + ' ' + rpkm)
+      # Running for all data from site D1
+      ks = 0.873799
+      rpkm = (feature_count/ks)/gene_length
+      with open(result_file, "a") as file:
+       file.write(str(rpkm))
 
