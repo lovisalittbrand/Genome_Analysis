bcaf31ffd992704a2a2967ad8a3a40142a3e6e92
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-05-25 10:40:53 -0700

    analyze percentage expression

--- code/17_bin_expressed.py
+++ code/17_bin_expressed.py
@@ -2,10 +2,14 @@
 # This script sums all the mapped reads for each bin and saves the result into a text-file.
 
 bin_list = [1, 2, 6, 7, 12, 16, 19, 24, 26]
-result_file = 'result_bin_expressed.txt'
+result_file = 'result_gene_expression.txt'
+total_count_bins_D1 = 0
+total_count_bins_D3 = 0
+total_no_feature_bins_D1 = 0
+total_no_feature_bins_D3 = 0
 
 with open(result_file, "a") as file:
-    file.write('Bin_Site' + ' ' + 'Count' + '\n')
+    file.write('Bin_Site' + ' ' + 'Count' + ' ' + 'Count_no_features' + '\n')
 
 for i in bin_list:
     # Defining the files corresponding to the specific bin
@@ -18,6 +22,7 @@ for i in bin_list:
     # Opening counting-file from site D3 for a bin and adding all the mapped reads together
     with open(file, "r") as file:
         count_D3 = 0
+        count_total = 0
 
         for line in file:
             # Looking at features that don't start with _
@@ -28,14 +33,23 @@ for i in bin_list:
                 # Adding number of mapped reads together
                 count_D3 = count_D3 + int(split_line[1])
 
+            else:
+                split_line = line.split()
+                if split_line[0] == '__no_feature':
+                    no_features = split_line[1]
+
+        total_count_bins_D3 = total_count_bins_D3 + int(count_D3)
+        total_no_feature_bins_D3 = total_no_feature_bins_D3 + int(no_features)
+
                 # Add result to text file
         with open(result_file, "a") as file:
             file.write('bin_' + str(i) + '_D3')
-            file.write(' ' + str(count_D3) + '\n')
+            file.write(' ' + str(count_D3) + ' ' + str(no_features) + '\n')
 
     # Opening counting-file from site D1 for a bin and adding all the mapped reads together
     with open(file2, "r") as file2:
         count_D1 = 0
+        count_total = 0
 
         for line in file2:
             # Looking at features that don't start with _
@@ -46,7 +60,28 @@ for i in bin_list:
                 # Adding number of mapped reads together
                 count_D1 = count_D1 + int(split_line[1])
 
+            else:
+                split_line = line.split()
+                if split_line[0] == '__no_feature':
+                    no_features = split_line[1]
+
+        total_count_bins_D1 = total_count_bins_D1 + int(count_D1)
+        total_no_feature_bins_D1 = total_no_feature_bins_D1 + int(no_features)
+
                 # Add result to text file
         with open(result_file, "a") as file:
             file.write('bin_' + str(i) + '_D1')
-            file.write(' ' + str(count_D1) + '\n')
+            file.write(' ' + str(count_D1) + ' ' + str(no_features) + '\n')
+
+with open(result_file, "a") as file:
+    file.write('\n')
+    file.write('Total_count_gene_D1' + ' ' + str(total_count_bins_D1) + '\n')
+    file.write('Total_count_no_feature_D1' + ' ' + str(total_no_feature_bins_D1) + '\n')
+    file.write('% contigs' + ' ' + str(float((total_count_bins_D1+total_no_feature_bins_D1)/873799)) + '\n')
+    file.write('% genes' + ' ' + str(float(total_count_bins_D1/873799)) + '\n')
+
+    file.write('\n')
+    file.write('Total_count_gene_D3' + ' ' + str(total_count_bins_D3) + '\n')
+    file.write('Total_count_no_feature_D3' + ' ' + str(total_no_feature_bins_D3) + '\n')
+    file.write('% contigs' + ' ' + str(float((total_count_bins_D3+total_no_feature_bins_D3)/732822)) + '\n')
+    file.write('% genes' + ' ' + str(float(total_count_bins_D3/732822)) + '\n')
